1b75bb09b5780dd864c4cabbc0ff83113671376a
Paul Killian <psk65lava@gmail.com>
2020-07-29 10:32:32 -0700

    get-word-count

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function getWordCount(sentence) {
+  var counter = 0
+  for(var i = 0; i < sentence.length; i++) {
+  if(sentence[i] === " ") {
+    counter += 1;
+
+  }
+ }
+  return counter + 1
+}
